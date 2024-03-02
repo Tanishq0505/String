8e279cac69e5a4ae651126d4e15939be5f0da660
Tanishq0505 <126601008+Tanishq0505@users.noreply.github.com>
2024-03-02 11:57:17 +0530

    Create 23_Roman_Number_to_Integer.cpp

---
+++ 23_Roman_Number_to_Integer.cpp
@@ -0,0 +1,52 @@
+//  Roman Number to Integer (gfg & Leetcode )
+
+// Given a string in roman no format (s)  your task is to convert it to an integer . Various symbols and their values are given below.
+// I 1
+// V 5
+// X 10
+// L 50
+// C 100
+// D 500
+// M 1000
+
+// Example 1:
+
+// Input:
+// s = V
+// Output: 5
+
+// Example 2:
+
+// Input:
+// s = III
+// Output: 3
+
+// https://youtu.be/dlATMslQ6Uc
+// Q23 in string notes
+
+class Solution {
+public:
+    int romanToInt(string s) {
+          unordered_map<char, int> m;
+  
+        m['I'] = 1;
+        m['V'] = 5;
+        m['X'] = 10;
+        m['L'] = 50;
+        m['C'] = 100;
+        m['D'] = 500;
+        m['M'] = 1000;
+        
+        int ans = 0;
+        
+        for(int i = 0; i < s.length(); i++){
+            if(m[s[i]] < m[s[i+1]]){
+                ans -= m[s[i]];
+            }
+            else{
+                ans += m[s[i]];
+            }
+        }
+        return ans;
+    }
+};
